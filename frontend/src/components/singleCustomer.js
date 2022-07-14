8c8025a8edb079396779db72695edd9da14774a9
Topi Kanninen <topi.kanninen@sci.fi>
2022-07-14 16:26:20 +0300

    edit view in single customer done

--- frontend/src/components/singleCustomer.js
+++ frontend/src/components/singleCustomer.js
@@ -1,9 +1,16 @@
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
 import savedService from '../services/savedCustomers'
 
 const SingleCustomer = ({customer}) => {
     let savedCustomers = useSelector(state => state.saved)
+    const [customerName, setCustomerName] = useState(customer.name)
+    const [customerStreetAddress, setCustomerStreetAddress] = useState(customer.address.streetAddress)
+    const [customerCity, setCustomerCity] = useState(customer.address.city)
+    const [customerState, setCustomerState] = useState(customer.address.state)
+    const [customerZip, setCustomerZip] = useState(customer.address.zip)
+    const [visible, setVisible] = useState(true)
     
     const navigate = useNavigate()
 
@@ -18,6 +25,12 @@ const SingleCustomer = ({customer}) => {
 
     }
 
+    const handleEdit = (event) => {
+        event.preventDefault()
+        console.log('toimii')
+        
+    }
+
 
     const customerStyle = {
         paddingTop: 10,
@@ -26,6 +39,37 @@ const SingleCustomer = ({customer}) => {
         borderWidth: 1,
         marginBottom: 5,
         marginTop: 5
+    }
+
+    if (!visible) {
+        return(
+            <div style={customerStyle}>
+                <form onSubmit={handleEdit}>
+                    <div>
+                        Name:
+                        <input id='name' type='text' value={customerName} name="Name" onChange={(event) => setCustomerName(event.target.value)}/>
+                    </div>
+                    <div>
+                        Street Address:
+                        <input id='street' type='text' value={customerStreetAddress} name="Street" onChange={(event => setCustomerStreetAddress(event.target.value))}/>
+                    </div>
+                    <div>
+                        City:
+                        <input id='city' type='text' value={customerCity} name="City" onChange={(event) => setCustomerCity(event.target.value)}/>
+                    </div>
+                    <div>
+                        State:
+                        <input id='state' type='text' value={customerState} name="State" onChange={(event) => setCustomerState(event.target.value)}/>
+                    </div>
+                    <div>
+                        Zip:
+                        <input id='zip' type='text' value={customerZip} name="Zip" onChange={(event) => setCustomerZip(event.target.value)}/>
+                    </div>
+                    <button id='edit' type='submit'>tallenna</button>
+                </form>
+                <button onClick={() => setVisible(!visible)}>takaisin</button>
+            </div>
+        )
     }
 
     return (
@@ -40,7 +84,7 @@ const SingleCustomer = ({customer}) => {
                 <div>{customer.address.zip}</div>
             </ul>
             </div>
-            <button>muokkaa</button>
+            <button onClick={() => setVisible(!visible)}>muokkaa</button>
             <button onClick={() => handleDelete(customer.id)}>poista</button>
         </div>
     )
